d47f7717e39a5dc0ad96a77e5d93de02e1258272
James Gardiner <jamesgardiner075@gmail.com>
2017-04-05 19:49:59 +0100

    Add routes to allow signup and login

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,17 +12,47 @@ router.get('/login', function(req, res, next) {
                         message: req.flash('loginMessage') });
 });
 
+/* POST login */
+router.post('/login', passport.authenticate('local-login', {
+  // Success go to Profile Page / Fail go to login page
+  sucessRedirect : '/profile',
+  failureRedirect : '/login',
+  failureFlash : true
+}));
+
 /* GET Signup page. */
 router.get('/signup', function(req, res, next){
   res.render('signup', { title: 'Signup Page',
                          message: req.flash('signupMessage')});
 });
 
+/* POST signup */
+router.post('/signup', passport.authenticate('local-signup', {
+  // Sucess go to Profile page / Faile go to Signup page
+  sucessRedirect : '/profile',
+  failureRedirect : '/signup',
+  failureFlash : true
+}));
+
 /* GET Profile page. */
-router.get('/profile', function(req, res, next) {
+router.get('/profile', isLoggedIn, function(req, res, next) {
   res.render('profile', { title: 'Profile Page', user: req.user,
                           avatar: gravatar.url(req.user.email, {s: '100',
                           r: 'x', d: 'retro'}, true)});
 });
+
+/* check if user is logged in */
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('login');
+}
+
+/* GET Logout Page */
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
 
 module.exports = router;
